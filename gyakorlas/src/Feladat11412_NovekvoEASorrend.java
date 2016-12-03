a6aa1b3c467bf3ecd4cbe70aafbfa6c348d884e1
Mate Retsagi <retsagimate@gmail.com>
2016-12-03 20:13:32 +0100

    11412-es módosítva

--- gyakorlas/src/Feladat11412_NovekvoEASorrend.java
+++ gyakorlas/src/Feladat11412_NovekvoEASorrend.java
@@ -1,21 +1,33 @@
 
 public class Feladat11412_NovekvoEASorrend {
-  public static int[] myArray = {4, 8, 34, 47, 53, 65, 99};
-  
-  public static boolean isAscending(int[] array) {
 
-    int swap = 0;
-    for (int i = array.length; i > 0; i--) {
-      for (int j = 0; j < i - 1; j++) {
-        if (array[j] > array[j + 1]) {
-          swap++;
-        }        
-      }      
+  public static int[] myArray = {14, 8, 34, 47, 53, 65, 99};
+
+  public static boolean isAscending(int[] array) {
+//    for (int i = 0; i < array.length - 1; i++) {
+//      if (array[i] > array[i + 1]) {
+//        return false;
+//      }
+//    }
+//    return true;
+    int i = 0;
+    while(i < array.length - 1 && array[i] <= array[i+1]) {
+      i++;
     }
-    return swap == 0;
-  }
-  
-  public static void main(String[] args) {
+    return i == array.length - 1;
+
+//    int swap = 0;
+//    for (int i = array.length; i > 0; i--) {
+//      for (int j = 0; j < i - 1; j++) {
+//        if (array[j] > array[j + 1]) {
+//          swap++;
+//        }        
+//      }      
+//    }
+//    return swap == 0;
+}
+
+public static void main(String[] args) {
     System.out.println("A tömbelemek " + (isAscending(myArray) ? "" : "nem ") + "növekvő sorrendbe vannak rendezve");
   }
 }
